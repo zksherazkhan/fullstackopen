15bd7d1d8c4627eee480abb1b62bf3434d993852
sheraz khan <zkkhansheraz3241@gmail.com>
2025-03-31 22:57:27 +0200

    a

--- part1/courseinfo/latest/src/App.jsx
+++ part1/courseinfo/latest/src/App.jsx
@@ -20,7 +20,6 @@ const App = () => {
         {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
