6cf15969837cf6a7089e38ac9fff197abc8d0136
sheraz khan <zkkhansheraz3241@gmail.com>
2025-04-02 21:53:19 +0200

    used array object

--- part1/courseinfo/latest/src/App.jsx
+++ part1/courseinfo/latest/src/App.jsx
@@ -1,25 +1,30 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "state of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      {parts.map((val) => {
+        return (
+          <p key={val.name}>
+            {val.name} {val.exercises}
+          </p>
+        );
+      })}
     </div>
   );
 };
