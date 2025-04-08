bbebc78866487543b295676e861b8e12c055f17f
sheraz khan <zkkhansheraz3241@gmail.com>
2025-04-08 23:07:25 +0200

    unicafe Exercise completed

--- part1/unicafe/my-app/src/App.jsx
+++ part1/unicafe/my-app/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allfeedback, setAllFeedback] = useState(0);
+
+  useEffect(() => {
+    setAllFeedback(good + neutral + bad);
+  }, [good, neutral, bad]);
 
   return (
     <>
@@ -13,13 +19,60 @@ function App() {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <br />
       <h1>Statistics</h1>
-      <p>good: {good} </p>
+      {/* <p>good: {good} </p>
       <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <br />
-      <p>All : {good + neutral + bad}</p>
+      <p>bad: {bad}</p> */}
+      <Statistics
+        allfeedback={allfeedback}
+        goodPercentage={good}
+        good={good}
+        bad={bad}
+        neutral={neutral}
+      />
     </>
   );
 }
 
 export default App;
+
+const Statistics = ({ allfeedback, goodPercentage, good, bad, neutral }) => {
+  if (allfeedback === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <tr>
+            <td>All :</td>
+            <td>{allfeedback}</td>
+          </tr>
+          <tr>
+            <td>Average : </td>
+            <td>{allfeedback / 3}</td>
+          </tr>
+          <tr>
+            <td>Positive :</td>
+            <td>{(goodPercentage / allfeedback) * 100}%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
